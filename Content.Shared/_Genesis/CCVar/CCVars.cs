43290d1c5c2edcb9fd37a500178cc6fb1ab84f8b
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2024-10-10 21:21:37 +0300

    Create CCVars.cs

---
+++ Content.Shared/_Genesis/CCVar/CCVars.cs
@@ -0,0 +1,20 @@
+using Robust.Shared;
+using Robust.Shared.Configuration;
+
+namespace Content.Shared._Genesis.CCVar;
+
+// ReSharper disable once InconsistentNaming
+[CVarDefs]
+public sealed class CCVars
+{
+    /*
+     * Ghost Respawn
+     */
+
+    public static readonly CVarDef<float> GhostRespawnTime =
+        CVarDef.Create("ghost.respawn_time", 15f, CVar.SERVER | CVar.REPLICATED);
+
+    public static readonly CVarDef<int> GhostRespawnMaxPlayers =
+        CVarDef.Create("ghost.respawn_max_players", 40, CVar.SERVERONLY);
+
+}
