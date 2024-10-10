e627c21ace6c19439d521c9fb59b37e503846c6c
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2024-10-10 21:28:25 +0300

    Update GhostReJoinSystem.cs

--- Content.Server/_Genesis/Ghost/GhostReJoinSystem.cs
+++ Content.Server/_Genesis/Ghost/GhostReJoinSystem.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using Content.Server.Chat.Managers;
 using Content.Server.EUI;
 using Content.Server.GameTicking;
@@ -24,7 +24,9 @@ using Robust.Shared.Network;
 using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
+
 namespace Content.Server._Genesis.Ghost;
+
 public sealed class GhostReJoinSystem : SharedGhostReJoinSystem
 {
     [Dependency] private readonly IPrototypeManager _prototype = default!;
@@ -43,19 +45,25 @@ public sealed class GhostReJoinSystem : SharedGhostReJoinSystem
     [Dependency] private readonly SharedHumanoidAppearanceSystem _appearance = default!;
     [Dependency] private readonly StationJobsSystem _stationJobs = default!;
     [Dependency] private readonly SharedJobSystem _jobs = default!;
+
     public override void Initialize()
     {
         base.Initialize();
+
         SubscribeLocalEvent<RoundRestartCleanupEvent>(ResetDeathTimes);
         SubscribeLocalEvent<PlayerSpawnCompleteEvent>(HandleUserCharacter);
+
         _configurationManager.OnValueChanged(CCVars.GhostRespawnMaxPlayers,
             ghostRespawnMaxPlayers =>
             {
                 _ghostRespawnMaxPlayers = ghostRespawnMaxPlayers;
             },
             true);
+
+
         _console.RegisterCommand("return_to_round", ReturnToRoundCommand, ReturnToRoundCompletion);
     }
+
     private void HandleUserCharacter(PlayerSpawnCompleteEvent ev)
     {
         _usedInRound.TryAdd(ev.Player.UserId, []);
@@ -66,13 +74,16 @@ public sealed class GhostReJoinSystem : SharedGhostReJoinSystem
             Log.Error($"GetPreferences returned null for player {ev.Player}");
             return;
         }
+
         if (!prefs.TryIndexOfCharacter(ev.Profile, out var idx))
         {
             Log.Warning($"GetPreferences returned no character for player {ev.Player}");
             return;
         }
+
         list.Add(idx);
     }
+
     public void OnJoinSelected(GhostReJoinEui ui, Entity<GhostComponent> ent, ref GhostReJoinCharacterMessage args)
     {
         if (TerminatingOrDeleted(ent))
@@ -80,6 +91,7 @@ public sealed class GhostReJoinSystem : SharedGhostReJoinSystem
             _euiManager.CloseEui(ui);
             return;
         }
+
         if (_playerManager.PlayerCount >= _ghostRespawnMaxPlayers)
         {
             SendChatMsg(ui.Player,
@@ -87,6 +99,7 @@ public sealed class GhostReJoinSystem : SharedGhostReJoinSystem
             );
             return;
         }
+
         var station = GetEntity(args.Station);
         if (!TryComp<StationJobsComponent>(station, out var jobs))
         {
@@ -100,16 +113,21 @@ public sealed class GhostReJoinSystem : SharedGhostReJoinSystem
             );
             return;
         }
+
         var prefs = _prefs.GetPreferencesOrNull(ui.Player.UserId);
         if(prefs == null)
             return;
+
         if(!prefs.Characters.TryGetValue(args.Id, out var character))
             return;
+
         _euiManager.CloseEui(ui);
         _eUi.Remove(ui.Player.UserId);
         _deathTime.Remove(ui.Player.UserId);
+
         SpawnPlayerOnStation((station,jobs),ui.Player, (HumanoidCharacterProfile)character);
     }
+
     public void OnJoinRandom(GhostReJoinEui ui, Entity<GhostComponent> ent, ref GhostReJoinRandomMessage args)
     {
         if (TerminatingOrDeleted(ent))
@@ -117,6 +135,7 @@ public sealed class GhostReJoinSystem : SharedGhostReJoinSystem
             _euiManager.CloseEui(ui);
             return;
         }
+
         if (_playerManager.PlayerCount >= _ghostRespawnMaxPlayers)
         {
             SendChatMsg(ui.Player,
@@ -124,6 +143,7 @@ public sealed class GhostReJoinSystem : SharedGhostReJoinSystem
             );
             return;
         }
+
         var station = GetEntity(args.Station);
         if (!TryComp<StationJobsComponent>(station, out var jobs))
         {
@@ -137,43 +157,57 @@ public sealed class GhostReJoinSystem : SharedGhostReJoinSystem
             );
             return;
         }
+
         var character = HumanoidCharacterProfile.Random();
+
         _euiManager.CloseEui(ui);
         _eUi.Remove(ui.Player.UserId);
         _deathTime.Remove(ui.Player.UserId);
+
         SpawnPlayerOnStation((station,jobs),ui.Player, character);
     }
+
     private void SpawnPlayerOnStation(Entity<StationJobsComponent> station, ICommonSession player, HumanoidCharacterProfile character)
     {
         _deathTime.Remove(player.UserId);
+
         var jobPrototype = _prototype.Index<JobPrototype>(station.Comp.OverflowJobs.First());
         var job = new JobComponent { Prototype = jobPrototype.ID };
+
         var newMind = _mind.CreateMind(player.UserId, character.Name);
         _mind.SetUserId(newMind, player.UserId);
         _roles.MindAddRole(newMind, job, silent: false);
+
         _playTimeTrackings.PlayerRolesChanged(player);
+
         var spawnEv = new PlayerSpawningEvent(job, character, station);
         RaiseLocalEvent(spawnEv);
         DebugTools.Assert(spawnEv.SpawnResult is { Valid: true } or null);
+
         var mob = spawnEv.SpawnResult!.Value;
         _appearance.LoadProfile(mob, character);
         _mind.TransferTo(newMind, mob);
         _stationJobs.TryAssignJob(station, jobPrototype, player.UserId);
+
         var jobName = _jobs.MindTryGetJobName(newMind);
         _adminLogger.Add(LogType.Mind,
             LogImpact.Extreme,
             $"гост респавн {player.Name} late joined as {character.Name:characterName} on station {Name(station):stationName} with {ToPrettyString(mob):entity} as a {jobName:jobNam}.");
+
         if (TryComp(station, out MetaDataComponent? metaData))
         {
             _chatManager.DispatchServerMessage(player,
                 Loc.GetString("job-greet-station-name", ("stationName", metaData.EntityName)));
         }
+
         var ev = new PlayerSpawnCompleteEvent(mob, player, jobPrototype.ID, true, true, 0, station, character);
         RaiseLocalEvent(ev);
+
         SendChatMsg(player,
             Loc.GetString("ghost-respawn-window-rules-footer")
         );
     }
+
     private List<EntityUid> GetSpawnableStations()
     {
         var spawnableStations = new List<EntityUid>();
@@ -186,40 +220,52 @@ public sealed class GhostReJoinSystem : SharedGhostReJoinSystem
                 continue;
             spawnableStations.Add(uid);
         }
+
         return spawnableStations;
     }
+
     public GhostReJoinInterfaceState UpdateUserInterface(Entity<GhostComponent> ent)
     {
         var msg = new GhostReJoinInterfaceState();
+
         if(!_actorSystem.TryGetSession(ent, out var session))
             return msg;
+
         var prefs = _prefs.GetPreferencesOrNull(session!.UserId);
+
         if(prefs == null)
             return msg;
+
         _usedInRound.TryAdd(session.UserId, []);
         var usedByUser = _usedInRound[session.UserId];
+
         msg.Characters.AddRange(
             prefs.Characters
                 .Where(x => !usedByUser.Contains(x.Key))
                 .Select(x => new GhostReJoinCharacter(x.Key, x.Value.Name))
             );
+
         msg.Stations.AddRange(
             GetSpawnableStations().Select(s => new GhostReJoinStation(GetNetEntity(s), Name(s)))
             );
+
         return msg;
     }
+
     private CompletionResult ReturnToRoundCompletion(IConsoleShell shell, string[] args)
     {
         return CompletionResult.Empty;
     }
+
     [AnyCommand]
     private void ReturnToRoundCommand(IConsoleShell shell, string argstr, string[] args)
     {
-        if (shell.Player?.AttachedEntity is not { } entity || !TryComp<GhostComponent>(entity, out var ghostComponent))
+        if (shell.Player?.AttachedEntity is not { } ghost || !TryComp<GhostComponent>(ghost, out var ghostComponent))
         {
             shell.WriteError("This command can only be ran by a player with an attached entity.");
             return;
         }
+
         if (_playerManager.PlayerCount >= _ghostRespawnMaxPlayers)
         {
             SendChatMsg(shell.Player,
@@ -227,13 +273,23 @@ public sealed class GhostReJoinSystem : SharedGhostReJoinSystem
             );
             return;
         }
+
         var userId = shell.Player.UserId;
+
         if (!_deathTime.TryGetValue(userId, out var deathTime))
         {
             _deathTime[userId] = ghostComponent.TimeOfDeath;
             deathTime = ghostComponent.TimeOfDeath;
         }
+
+        if (deathTime != ghostComponent.TimeOfDeath)
+        {
+            _ghostSystem.SetTimeOfDeath(ghost, deathTime, ghostComponent);
+            Dirty(ghost, ghostComponent);
+        }
+
         var timeOffset = _gameTiming.CurTime - deathTime;
+
         if (timeOffset >= _ghostRespawnTime)
         {
             if (_eUi.ContainsKey(userId))
@@ -241,39 +297,30 @@ public sealed class GhostReJoinSystem : SharedGhostReJoinSystem
                 _euiManager.CloseEui(_eUi[userId]);
                 _eUi.Remove(userId);
             }
-            _eUi.Add(userId, new GhostReJoinEui(this, (entity, ghostComponent)));
+            _eUi.Add(userId, new GhostReJoinEui(this, (ghost, ghostComponent)));
             _euiManager.OpenEui(_eUi[userId], shell.Player);
             _eUi[userId].StateDirty();
-            /*
-            _deathTime.Remove(userId);
-            _adminLogger.Add(LogType.Mind,
-                LogImpact.Extreme,
-                $"{shell.Player.Channel.UserName} вернулся в лобби посредством гост респавна.");
-            SendChatMsg(shell.Player,
-                Loc.GetString("ghost-respawn-window-rules-footer")
-            );
-            _gameTicker.Respawn(shell.Player);
-            */
+
             return;
         }
+
         SendChatMsg(shell.Player,
             Loc.GetString("ghost-respawn-time-left", ("time", (_ghostRespawnTime - timeOffset).ToString()))
         );
     }
+
     private int _ghostRespawnMaxPlayers;
     private readonly Dictionary<NetUserId, TimeSpan> _deathTime = new();
     private readonly Dictionary<NetUserId, HashSet<int>> _usedInRound = new();
     private readonly Dictionary<NetUserId, GhostReJoinEui> _eUi = new();
+
     private void ResetDeathTimes(RoundRestartCleanupEvent ev)
     {
         _deathTime.Clear();
         _usedInRound.Clear();
-        foreach (var (_,joinEui) in _eUi)
-        {
-            _euiManager.CloseEui(joinEui);
-        }
         _eUi.Clear();
     }
+
     private void SendChatMsg(ICommonSession sess, string message)
     {
         _chatManager.ChatMessageToOne(Shared.Chat.ChatChannel.Server,
@@ -284,17 +331,21 @@ public sealed class GhostReJoinSystem : SharedGhostReJoinSystem
             sess.Channel,
             Color.Red);
     }
-    public void AttachGhost(EntityUid? ghost, ICommonSession? mindSession)
+
+    public void AttachGhost(EntityUid ghost, ICommonSession? mindSession)
     {
         if(mindSession == null)
             return;
+
         if(!_deathTime.ContainsKey(mindSession.UserId))
             _deathTime[mindSession.UserId] = _gameTiming.CurTime;
+
         Log.Debug($"Attach time {_deathTime[mindSession.UserId]} to ghost {ghost:entity}");
+
         if (TryComp<GhostComponent>(ghost, out var ghostComponent))
         {
-            _ghostSystem.SetTimeOfDeath(ghost.Value, _deathTime[mindSession.UserId], ghostComponent);
-            Dirty(ghost.Value, ghostComponent);
+            _ghostSystem.SetTimeOfDeath(ghost, _deathTime[mindSession.UserId], ghostComponent);
+            Dirty(ghost, ghostComponent);
         }
     }
 }
