5e1e1de6b63732ac00f3825abd77bea29d67160f
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2024-10-10 21:23:16 +0300

    Update SharedGhostReJoinSystem.cs

--- Content.Shared/_Genesis/Ghost/SharedGhostReJoinSystem.cs
+++ Content.Shared/_Genesis/Ghost/SharedGhostReJoinSystem.cs
@@ -1,20 +1,25 @@
-﻿using Content.Shared._Genesis.CCCVars;
+using Content.Shared._Genesis.CCVar;
 using Robust.Shared.Configuration;
 using Robust.Shared.Timing;
+
 namespace Content.Shared._Genesis.Ghost;
+
 public abstract class SharedGhostReJoinSystem : EntitySystem
 {
     [Dependency] protected readonly IConfigurationManager _configurationManager = default!;
     [Dependency] protected readonly IGameTiming _gameTiming = default!;
+
     public override void Initialize()
     {
         base.Initialize();
-        _configurationManager.OnValueChanged(CVar.GhostRespawnTime,
+
+        _configurationManager.OnValueChanged(CCVars.GhostRespawnTime,
             ghostRespawnTime =>
             {
                 _ghostRespawnTime = TimeSpan.FromMinutes(ghostRespawnTime);
             },
             true);
     }
+
     protected TimeSpan _ghostRespawnTime = new(0, 30, 0);
 }
