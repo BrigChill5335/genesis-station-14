566d3dd1bb34f296c05e301cac6ac96068286479
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2024-10-13 17:11:53 +0300

    Update CCVars.cs

--- Content.Shared/_Genesis/CCVar/CCVars.cs
+++ Content.Shared/_Genesis/CCVar/CCVars.cs
@@ -6,60 +6,6 @@ namespace Content.Shared._Genesis.CCVar;
 [CVarDefs]
 public sealed class CCVars
 {
-    /// <summary>
-    ///     Controls if the connections queue is enabled. If enabled stop kicking new players after `SoftMaxPlayers` cap and instead add them to queue.
-    /// </summary>
-    public static readonly CVarDef<bool>
-        QueueEnabled = CVarDef.Create("queue.enabled", false, CVar.SERVERONLY);
-
-    public static readonly CVarDef<bool>
-        QueueAltEnabled = CVarDef.Create("queue.alt_servers", false, CVar.SERVERONLY);
-
-    /*
-     * Discord Auth
-     */
-
-    /// <summary>
-    ///     Enabled Discord linking, show linking button and modal window
-    /// </summary>
-    public static readonly CVarDef<bool> DiscordAuthEnabled =
-        CVarDef.Create("discord_auth.enabled", false, CVar.SERVER | CVar.REPLICATED);
-
-    /// <summary>
-    ///     URL of the Discord auth server API
-    /// </summary>
-    public static readonly CVarDef<string> DiscordAuthApiUrl =
-        CVarDef.Create("discord_auth.api_url", "", CVar.SERVERONLY);
-
-    /// <summary>
-    ///     Secret key of the Discord auth server API
-    /// </summary>
-    public static readonly CVarDef<string> DiscordAuthApiKey =
-        CVarDef.Create("discord_auth.api_key", "", CVar.SERVERONLY | CVar.CONFIDENTIAL);
-
-    public static readonly CVarDef<bool> DiscordAuthIsOptional =
-        CVarDef.Create("discord_auth.is_opt", false, CVar.SERVER | CVar.REPLICATED);
-
-    /**
-     * Sponsors
-     */
-
-    /// <summary>
-    ///     URL of the sponsors server API.
-    /// </summary>
-    public static readonly CVarDef<string> SponsorsApiUrl =
-        CVarDef.Create("sponsor.api_url", "", CVar.SERVERONLY);
-
-    public static readonly CVarDef<string> SponsorsSelectedGhost =
-        CVarDef.Create("sponsor.ghost", "", CVar.REPLICATED | CVar.CLIENT | CVar.ARCHIVE);
-
-
-    public static readonly CVarDef<bool>
-        EconomyWagesEnabled = CVarDef.Create("economy.wages_enabled", true, CVar.SERVERONLY);
-
-    public static readonly CVarDef<bool>
-        WhitelistRolesEnabled = CVarDef.Create("game.whitelist_role_enabled", true, CVar.SERVER | CVar.REPLICATED);
-
     /*
      * Ghost Respawn
      */
