a487d1c64cf5d56bec7ce5072d15eb0e60e874fa
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2025-09-15 13:11:05 +0300

    del pseudoitem (myb not full) (#95)
    * sdaasda
    
    * Update SharedPseudoItemSystem.cs
    
    * Update PseudoItemComponent.cs
    
    * Update felinid.yml

--- Content.Shared/_Genesis/Item/PseudoItemInsertDoAfterEvent.cs
+++ Content.Shared/_Genesis/Item/PseudoItemInsertDoAfterEvent.cs
@@ -1,9 +1,9 @@
-﻿using Content.Shared.DoAfter;
-using Robust.Shared.Serialization;
+//﻿using Content.Shared.DoAfter;
+//﻿using Robust.Shared.Serialization;
 
-namespace Content.Shared._Genesis.Item;
+//﻿namespace Content.Shared._Genesis.Item;
 
-[Serializable, NetSerializable]
-public sealed partial class PseudoItemInsertDoAfterEvent : SimpleDoAfterEvent
-{
-}
+//﻿[Serializable, NetSerializable]
+//﻿public sealed partial class PseudoItemInsertDoAfterEvent : SimpleDoAfterEvent
+//﻿{
+//﻿}
