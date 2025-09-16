f0676e8db02bc37c2c43c426c068d61b6431562a
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2025-09-16 07:29:16 +0300

    Update EscapeInventorySystem.cs

--- Content.Server/Resist/EscapeInventorySystem.cs
+++ Content.Server/Resist/EscapeInventorySystem.cs
@@ -9,6 +9,7 @@ using Content.Shared.Movement.Events;
 using Content.Shared.Resist;
 using Content.Shared.Storage;
 using Robust.Shared.Containers;
+using Content.Shared._Genesis.Carrying;
 
 namespace Content.Server.Resist;
 
@@ -19,6 +20,7 @@ public sealed class EscapeInventorySystem : EntitySystem
     [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
     [Dependency] private readonly ActionBlockerSystem _actionBlockerSystem = default!;
     [Dependency] private readonly SharedHandsSystem _handsSystem = default!;
+    [Dependency] private readonly SharedCarryingSystem _carryingSystem = default!;
 
     public override void Initialize()
     {
