14c9e03ff969795bf554d47303bef93d07161bc1
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2025-09-16 15:34:14 +0300

    rename

--- Content.Server/_Genesis/WoundLicking/WoundLickingSystem.cs
+++ Content.Server/_Genesis/WoundLicking/WoundLickingSystem.cs
@@ -14,7 +14,7 @@ using Content.Shared.Body.Components;
 using WoundLickingActionEvent = Content.Shared._Genesis.WoundLicking.WoundLickingActionEvent;
 using WoundLickingDoAfterEvent = Content.Shared._Genesis.WoundLicking.WoundLickingDoAfterEvent;
 
-namespace Content.Server.Felinid
+namespace Content._Genesis.Server.Felinid
 {
     /// <summary>
     /// "Lick your or other felinid wounds. Reduce bleeding, but unsanitary and can cause diseases."
