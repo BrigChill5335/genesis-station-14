b8c258015e47cf8340a68bd0d07f1959742174b4
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2025-09-16 15:33:29 +0300

    Update WoundLickingEvents.cs

--- Content.Shared/_Genesis/WoundLicking/WoundLickingEvents.cs
+++ Content.Shared/_Genesis/WoundLicking/WoundLickingEvents.cs
@@ -2,7 +2,7 @@ using Content.Shared.Actions;
 using Content.Shared.DoAfter;
 using Robust.Shared.Serialization;
 
-namespace Content.Shared.Felinid;
+namespace Content._Genesis.Shared.Felinid;
 
 [Serializable, NetSerializable]
 public sealed partial class WoundLickingDoAfterEvent : SimpleDoAfterEvent
